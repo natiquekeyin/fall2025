b2050cdfd99a5ecd01a1f6a6dbe0ae3f7c9fffcf
Noman Atique <noman.atique@keyin.com>
2025-09-08 11:52:19 -0400

    Sep8

---
+++ JavaScript/Week1and2/script.js
@@ -0,0 +1,46 @@
+// console.log("Hello from HTML file");
+// console.log("Hello from HTML file");
+// console.log("Hello from HTML file");
+
+// let age = 2;
+// if (age > 18) {
+//   console.log("Welcome");
+// } else {
+//   console.log("Not allowed");
+// }
+
+// var x;
+// x = 10;
+
+// {
+//   var y;
+//   let z;
+//   z = 30;
+//   const t = 100;
+//   y = 20;
+//   console.log(x);
+//   console.log(t);
+//   t = 200;
+// }
+
+// console.log(y);
+// // console.log(t)
+
+// let x = 1;
+// x++;
+// x++;
+// console.log(x);
+
+// let x = 10;
+// console.log(x++);
+// console.log(x);
+
+// let x = 10;
+// console.log(x++); //10 11
+// console.log(++x); //12 12
+// console.log(x++); //12 13
+// console.log(++x);//14 14
+
+let x = 5;
+console.log(x--); //5  4
+console.log(x--); //4  3
