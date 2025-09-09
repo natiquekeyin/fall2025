1016e9631887783fca291d405e6397086bddd3ef
Noman Atique <noman.atique@keyin.com>
2025-09-09 11:29:18 -0400

    sep9

--- JavaScript/Week1and2/script.js
+++ JavaScript/Week1and2/script.js
@@ -41,6 +41,65 @@
 // console.log(x++); //12 13
 // console.log(++x);//14 14
 
-let x = 5;
-console.log(x--); //5  4
-console.log(x--); //4  3
+// let x = 5;
+// console.log(x--); //5  4
+// console.log(x--); //4  3
+
+// let x = 10;
+// console.log(x++); //10
+// console.log(++x); //12
+
+// let age = 19;
+
+// let message;
+// // age > 18 ? console.log("Hello") : console.log("Bye");
+// message = age > 18 ? "Hello" : "Bye";
+// console.log(message)
+
+// console.log("hello");
+// console.log("Keyin COllege");
+
+// let age = 18;
+// let message;
+
+// if (age >= 18) {
+//   message = "Hello";
+// } else {
+//   message = "BYE";
+// }
+
+// console.log(message);
+
+// let month = "Mar";
+
+// switch (month) {
+//   case "Jan":
+//     console.log("JANUARY");
+//     break;
+//   case "Feb":
+//     console.log("FEBRUARY");
+//     break;
+//   case "Mar":
+//     console.log("MARCH");
+//     break;
+//   default:
+//     console.log("WRONG INPUT - TRY AGAIN");
+// }
+
+// for loop
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// prints 10 - 1
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+for (let i = 1; i <= 3; i++) {
+  for (let j = i; j <= 3; j++) {
+    console.log(i, j);
+  }
+}
