4a4be5d434bef0b06a7bc2f3d9e81f5d7503aed0
Noman Atique <noman.atique@keyin.com>
2025-09-23 11:23:36 -0400

    sep23

--- JavaScript/Week3/scripts/script.js
+++ JavaScript/Week3/scripts/script.js
@@ -129,3 +129,85 @@
 // console.log(makeAbbr("Alan Smith"));
 
 // We will start with Arrays!
+
+// Declaring arrays:
+
+// let numbers = [1, 2, 4, 5];
+// let numbers2 = new Array(6, 7, 8, 9);
+
+// console.log(numbers[1]);
+// console.log(numbers2[0]);
+
+// Accessing elements in array:
+
+// let arr = [20, 35, 67, 88, 44, 44, 42, 45, 12, 78, 23, 56];
+// // console.log(arr[0], arr[1], arr[2], arr[3]);
+
+// console.log(`First number in array is: ${arr[0]}`);
+// console.log(`The last number in array is: ${arr[arr.length - 1]}`);
+
+// let position = [42.3332, -90.1234];
+
+// // 1. Index notation
+
+// // let lat = position[0];
+// // let lng = position[1];
+
+// // 2. Destructing the array values
+
+// let [lat, lng] = position;
+
+// console.log(lat, lng);
+
+// let dateString = `23/09/2025`;
+
+// let [day, , year] = dateString.split("/");
+
+// console.log(day, year);
+
+// let email = "alan@gmail.com";
+// let [, domain] = email.split("@");
+
+// console.log(`The user name is .. and the domain here is ${domain}`);
+
+let list = [1, 2, 3, 4];
+
+// create a new list that adds 3 to eaach iitem in the list [4,5,6,7]
+
+// let listCopy = [];
+
+// // Method 1: for loop
+
+// for (let i = 0; i < list.length; i++) {
+//   let element = list[i];
+//   element += 3;
+//   listCopy.push(element);
+// }
+
+// Method 2: forEach function
+
+// list.forEach(function (element) {
+//   listCopy.push(element + 3);
+// });
+
+// Method 3: By using map() funcion,
+
+// let listCopy = list.map(function (element) {
+//   return element + 3;
+// });
+
+// let listCopy = list.map((element) => element + 3);
+
+// let listCopy = list.filter((element) => element > 1);
+// console.log(list);
+// console.log(listCopy);
+
+let fruits = ["Apple", "Banana", "Orange", "Pineapple", "Oxy"];
+
+let newFruits = fruits.map((fruit) => fruit.startsWith("A"));
+let newFruits2 = fruits.filter((fruit) => fruit.startsWith("A"));
+let newFruits3 = fruits.forEach((fruit) => fruit.startsWith("A")); //We dont use forEach like this! WHY? becasue forEach does not automatically return anything!
+console.log(fruits);
+console.log(newFruits);
+console.log(newFruits2);
+console.log(newFruits3);
